4604724ed7089d47eb4a9da41c7b88ded1e5bb3c
wtsaygers1 <wtsaygers1@gmail.com>
2021-05-17 14:07:06 -0400

    rapid fire key commands

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 });
